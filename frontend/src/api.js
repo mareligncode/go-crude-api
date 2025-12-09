2856c363752aa504de0bd120a17103971149bfc6
marelign <yimermarelign@gmail.com>
2025-12-09 13:45:41 +0300

    selamgin commit

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,9 @@ import axios from 'axios';
 
 const API_URL = "http://localhost:8080";
 
-export const getUsers = () => axios.get(`${API_URL}/api/users`);
+export const getUsers = () => {
+    axios.get(`${API_URL}/api/users`);
+} 
 export const getUser = (id) => axios.get(`${API_URL}/api/users/${id}`);
 export const createUser = (data) => axios.post(`${API_URL}/api/users`, data);
 export const updateUser = (id, data) => axios.put(`${API_URL}/api/users/${id}`, data);
